620ad227f640d0c0c30ffa64195a460ef630d3b9
ZhangBoYu <617025999@qq.com>
2025-02-18 20:29:29 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+const selectedKeys = ref(["1"]);
+</script>
+
+<template>
+    <a-layout>
+      <a-layout-header>Header</a-layout-header>
+      <a-layout-content>Content</a-layout-content>
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
+</template>
+
+<style scoped>
+.test{
+  width: 100px;
+  height: 40px;
+  border: 1px solid saddlebrown;
+}
+</style>
+
---
+++ src/views/Editor.vue
@@ -0,0 +1,12 @@
+<template>
+    <div :style="{background: '#fff'}"></div>
+</template>
+
+
+<style scoped>
+div{
+    width: 100px;
+    height: 100px;
+}
+
+</style>
